7c7adebbc137d4517d871a55f31e187ea1dfb779
Lin <linlv@LindeMacBook-Pro.local>
2019-08-07 22:15:45 +0800

    数据渲染学习

--- pages/demo/demo.vue
+++ pages/demo/demo.vue
@@ -1,34 +1,13 @@
 <template>
 	<view>
-		<!-- <view class="view-box animated" hover-stay-time="800" hover-class="view-box-hover rubberBand">第一个box</view>
-		<text :selectable="true">这是text \n 这是text \n  这是text \n</text>
-		<view class="view-box" id="view2">第一个box</view> -->
-<!-- 		<view class="box">
-			<text>text组件</text>
-			<view>1</view>
-			<view>2</view>
-			<view>3</view>
-			<view>4</view>
-		</view> -->
-<<<<<<< HEAD
-		<view class="box">
-			<view class="box-item">1</view>
-			<view class="box-item">2</view>
-			<view class="box-item">3</view>
-			<!-- <view class="box-item">4</view>
-			<view class="box-item">5</view>
-			<view class="box-item">6</view> -->
+		<view :class="classname">
+			<view>{{username}}</view>
+			<view>{{userinfo.username}}</view>
+			<!-- <view>{{itemdata}}</view> -->
 		</view>
-=======
+		<button type="default" @tap="changeUsername('Alex')">修改名字</button>
+		<button type="default" @tap="changeClassname('box2')">修改</button>
 		
-<!-- 		<view class="box">
-			<view class="box-item">1</view>
-			<view class="box-item">2</view>
-			<view class="box-item">3</view>
-			<view class="box-item">4</view>
-		</view> -->
-		
->>>>>>> 20d98f25232b73522cd555134eea085b64ad0155
 	</view>
 
 </template>
@@ -37,142 +16,59 @@
 	export default {
 		data() {
 			return {
-				
+				username: "Tom",
+				userinfo:{
+					username:"Jack"
+				},
+				classname: "box"
 			}
 		},
-		methods: {
-			
+		methods:{
+			changeUsername:function(name){
+				this.userinfo.username = "aaa"+name
+			},
+			changeClassname:function(newclass){
+				this.classname = newclass
+			}
 		}
 	}
 </script>
 
 <style>
-/* .view-box{
-	width: 200upx;
-	height: 200upx;
-	background: #007AFF;
-	color: #FFFFFF;
-	margin: 100upx;
-}
-.view-box-hover{
-	background: red;
-}
-#view2{
-	background: yellow;
-	color: black;
-}
 .box{
-	width: 300upx;
-	height: 300upx;
-	background: #007AFF;
-	color: #FFFFFF;
-	margin: 100upx;
-	padding: 50upx;
-}
-.box view{
-	font-size: 40upx;
-} */
-/* .box>view:nth-of-type(1){
-	background: #09BB07;
-}
-.box>view:nth-of-type(2){
-	background: red;
-}
-.box>view:nth-of-type(3){
-	background: pink;
-} */
-/* .box>view:first-child{
-	background: red;
-}
-.box>view:last-child{
-	background: pink;
-} */
-
-/* .box>view:first-of-type{
-	background: red;
-}
-.box>view:last-of-type{
-	background: pink;
-} */
-/* .box>view:nth-of-type(2n-1){
-	background: red;
-}
-.box>view:nth-of-type(2n){
-	background: pink;
-} */
-/* .box>view:nth-of-type(even){
-	background: red;
-}
-.box>view:nth-of-type(odd){
-	background: pink;
-} */
-<<<<<<< HEAD
-.box{
-	border: 1upx solid #CCCCCC;
+	border: 1upx solid #333333;
 	height: 500upx;
 	width: 100%;
 	display: flex;
-	flex-direction: row;
-	/* align-items: center; */
-	/* justify-content: center; */
 }
-.box-item{
+.box2{
+	border: 1upx solid #1AAD19;
+	height: 500upx;
+	width: 100%;
+	display: flex;
+}
+.box>view{
 	background: #007AFF;
 	color: white;
-	height: 200upx;
-	/* width: 50%; */
-	/* width: 500upx; */
-	/* line-height: 200upx; */
-	/* text-align: center; */
-	flex: 1;
-	font-size: 30upx;
 	font-weight: bold;
+	font-size: 35upx;
 	display: flex;
+	flex: 1;
+	height: 500upx;
 	justify-content: center;
 	align-items: center;
-	
+	border: 1upx solid #FFFFFF;
 }
-.box-item:nth-of-type(odd){
-	background: #007AFF;
-}
-.box-item:nth-of-type(even){
+.box2>view{
 	background: #09BB07;
-}
-/* .box-item:first-of-type{
-	flex-shrink: 0;
-} */
-.box-item:nth-of-type(1){
-	flex: 1;
-}
-.box-item:nth-of-type(2){
-	flex: 2;
-	align-self: flex-end;
-}
-.box-item:nth-of-type(3){
-	flex: 1;
-=======
-
-/* .box{
-	border: 1upx solid #CCCCCC;
-	height: 500upx;
-	width: 100%;
-}
-.box-item{
-	background: #007AFF;
-	color: #FFFFFF;
-	height: 200upx;
-	width: 200upx;
-	line-height: 200upx;
-	font-size: 30upx;
+	color: white;
 	font-weight: bold;
-	text-align: center;
+	font-size: 35upx;
+	display: flex;
+	flex: 1;
+	height: 500upx;
+	justify-content: center;
+	align-items: center;
+	border: 1upx solid #FFFFFF;
 }
-.box-item:nth-of-type(odd){
-	background: #007AFF;
->>>>>>> 20d98f25232b73522cd555134eea085b64ad0155
-}
-.box-item:nth-of-type(even){
-	background: #09BB07;
-	
-} */
 </style>
