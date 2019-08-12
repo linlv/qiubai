b8338b3ac5c8df87c01a112c4a4791a49b54b56a
Lin <linlv@LindeMacBook-Pro.local>
2019-08-12 23:36:04 +0800

    列表渲染学习“
    git commit -m 列表渲染学习”

--- pages/demo/demo.vue
+++ pages/demo/demo.vue
@@ -1,26 +1,22 @@
 <template>
 	<view>
-		<!-- <view class="box" v-if="isshow">box</view>
-		<button type="default" @tap="changeshow()">隐藏</button> -->
-		
-		<!-- <view class="box" v-if="(age>20)">{{age>30?'中年人':'年轻人'}}</view>
-		<button type="default" @tap="changeAge()">修改</button> -->
-		
-		<!-- <view class="box" v-show="isshow">显示</view>
-		<button type="default" @tap="changeshow()">隐藏</button> -->
-		
-		<template v-if="isshow">
-			<view class="box">box</view>
-		</template>
-		<template v-else-if="isshow2">
-			<view class="box" style="background: #2C405A;">box2</view>
-		</template>
-		<template v-else>
-			<view class="box" style="background: #09BB07;">box2</view>
-		</template>
-		<button type="default" @tap="changeshow()">隐藏</button>
-		
+		<!-- 循环一维数组 -->
+		<!-- <view class="font" v-for="(val,index) in list1" :key=index>{{index}}-{{val}}</view> -->
+	<!-- <view class="font" v-for="(val,index) in list2" :key=index>
+	{{index}}-{{val.name}}-{{val.id}}
+	</view> -->
+	<!-- 循环二维数组 -->
+		<!-- <view class="font" v-for="(val1,index1) in list3" :key="index1">
+			<view class="font">{{val1.name}}</view>
+			<view class="font" v-for="(val2,index2) in val1.list" :key="index2">{{index2}}-{{val2}}</view>
+		</view> -->
+		<!-- 循环对象 -->
+		<!-- <view class="font" v-for="(val,key) in objlist" :key="key">{{key}}-{{val}}</view> -->
+		<block v-for="(val,key) in objlist" :key="key">
+			<view class="font">{{key}}-{{val}}</view>
+		</block>
 	</view>
+	
 
 </template>
 
@@ -28,31 +24,31 @@
 	export default {
 		data() {
 			return {
-				isshow:true,
-				age:10,
-				isshow2:true
+				list1:["篮球","足球","羽毛球"],
+				list2:[
+					{name:"篮球",id:"lanqiu"},
+					{name:"足球",id:"zuqiu"},
+					{name:"羽毛球",id:"yumaoqiu"}
+				],
+				list3:[
+					{name:"广东",list:["广州","深圳","佛山"]},
+					{name:"四川",list:["成都","乐山"]},
+				],
+				objlist:{
+					name1:"篮球",
+					name2:"足球",
+					name3:"羽毛球"
+				}
 			}
 		},
 		methods:{
-			changeshow:function(){
-				this.isshow = !this.isshow;
-			},
-			changeAge:function(){
-				this.age+=11;
-			}
+			
 		}
 	}
 </script>
 
 <style>
-.box{
-	background: #007AFF;
-	color: #FFFFFF;
+.font{
 	font-size: 50upx;
-	width: 350upx;
-	height: 350upx;
-	display: flex;
-	justify-content: center;
-	align-items: center;
 }
 </style>
