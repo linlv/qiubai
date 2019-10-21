c50d558da31f9d200fa5e612f4153990e5f330f4
Lin <linlv@LindeMacBook-Pro.local>
2019-10-21 22:08:08 +0800

    事件处理器

--- pages/demo/demo.vue
+++ pages/demo/demo.vue
@@ -1,54 +1,74 @@
 <template>
 	<view>
-		<!-- 循环一维数组 -->
-		<!-- <view class="font" v-for="(val,index) in list1" :key=index>{{index}}-{{val}}</view> -->
-	<!-- <view class="font" v-for="(val,index) in list2" :key=index>
-	{{index}}-{{val.name}}-{{val.id}}
-	</view> -->
-	<!-- 循环二维数组 -->
-		<!-- <view class="font" v-for="(val1,index1) in list3" :key="index1">
-			<view class="font">{{val1.name}}</view>
-			<view class="font" v-for="(val2,index2) in val1.list" :key="index2">{{index2}}-{{val2}}</view>
-		</view> -->
-		<!-- 循环对象 -->
-		<!-- <view class="font" v-for="(val,key) in objlist" :key="key">{{key}}-{{val}}</view> -->
-		<block v-for="(val,key) in objlist" :key="key">
-			<view class="font">{{key}}-{{val}}</view>
-		</block>
+		<!-- <view class="font">{{name}}</view>
+		<view class="box" @tap="clickEvent">按钮</view> -->
+		<view class="box1" @tap.stop="box1event()">
+			外面
+			<view class="box2" @tap.stop="box2event()">里面</view>
+		</view>
 	</view>
-	
-
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				list1:["篮球","足球","羽毛球"],
-				list2:[
-					{name:"篮球",id:"lanqiu"},
-					{name:"足球",id:"zuqiu"},
-					{name:"羽毛球",id:"yumaoqiu"}
-				],
-				list3:[
-					{name:"广东",list:["广州","深圳","佛山"]},
-					{name:"四川",list:["成都","乐山"]},
-				],
-				objlist:{
-					name1:"篮球",
-					name2:"足球",
-					name3:"羽毛球"
-				}
+				name:"哈哈哈"
 			}
 		},
 		methods:{
-			
+			clickEvent:function(){
+				this.name="xixixi"
+				console.log(this.name)
+			},
+			box1event:function(){
+				console.log("点击了外面")
+			},
+			box2event:function(){
+				console.log("点击了里面")
+				
+			}
 		}
 	}
 </script>
 
 <style>
+.box1{
+	width: 100%;
+	height: 500upx;
+	background: #007AFF;
+	color: #FFFFFF;
+	font-size: 40upx;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.box2{
+	width: 300upx;
+	height: 300upx;
+	background: #09BB07;
+	color: #FFFFFF;
+	font-size: 40upx;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.box{
+	background: #09BB07;
+	color: #FFFFFF;
+	width: 80%;
+	margin: auto;
+	height: 80upx;
+	font-size: 50upx;
+	border-radius: 30upx;
+	border: 1upx solid #EEEEEE;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 .font{
 	font-size: 50upx;
+	border: 1upx solid #CCCCCC;
+	padding: 20upx;
 }
 </style>
